e58f964befda1f58ddd4675e081e9b9ed4301320
chengyao <bigyao@139.com>
2023-02-23 15:07:37 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
-use Illuminate\Routing\ResponseFactory;
-use Illuminate\Support\Facades\Auth;
-
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        return view('welcome');
     }
 }
--- public/static/js/main.js
+++
@@ -1,5 +0,0 @@
-$(function() {
- if($("#premiumComparisonSidebar").height() > $("#nuhostcartcustom").height()){    
-    $("#nuhostcartcustom").height($("#premiumComparisonSidebar").height());
-}
-});
